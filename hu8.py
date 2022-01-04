590511692daa12c20a3fffd3173b5f19680e5a35
bettyballin <betty.ballin@yahoo.de>
2022-01-04 19:38:10 +0100

    hu8 finished

--- hu8.py
+++ hu8.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 import plotly.graph_objects as go
 import geoplotlib
+import plotly.express as px
 
 def vis():
 
@@ -62,45 +63,45 @@ def visualize():
     fig.show()
 
 def vis2():
-    locations = pd.read_csv("route2.csv", delimiter=";")
+    locations = pd.read_csv("route1.csv", delimiter=";")
     locations["Latitude"] = locations["Latitude"].apply(lambda x: float(x.replace(",", ".")))
     locations["Longitude"] = locations["Longitude"].apply(lambda x: float(x.replace(",", ".")))
 
+    #locations = locations.iloc[::20, :]
     scale = 5000
-    fig = go.Figure()
-    
+    fig = go.Figure(go.Scattermapbox(lon=locations["Longitude"],lat=locations["Latitude"],mode="markers+text",marker={"size":15,"color":"red"},name="Route",text=locations["Ort"],textfont={"color":"black","size":10},textposition="bottom right"))
+
     last_lon = locations['Longitude'][0]
     last_lat = locations['Latitude'][0]
+    #fig = px.line_mapbox(locations, lat="Latitude", lon="Longitude", text="Ort",zoom=5, height=900, width=700, title="Route des Weihnachtsmanns")
     for i in range(len(locations)-1):
-        if abs(last_lon-locations['Longitude'][i+1]) > 0.01 and abs(last_lat-locations['Latitude'][i+1]) > 0.01:
-            fig.add_trace(go.Scattergeo(
+        if True:#abs(last_lon-locations['Longitude'][i+1]) > 0.001 and abs(last_lat-locations['Latitude'][i+1]) > 0.001:
+            """fig.add_trace(go.Scattermapbox(
                 lon=[last_lon],
                 lat=[last_lat],
-                text=[" x"],
+                text=[locations["Ort"]],
                 textfont={"color": 'green',
                         "family": 'Droid Serif',
                         "size": 10},
                 textposition="top center",
                 name="Candidate Facility",
-                mode="markers",
+                mode="markers+lines",
                 marker=dict(
                     size=1,
                     color="red",
-                    line_color='blue',
-                    symbol = 'cross',
-                    sizemode='area')))
+                    symbol = '0',
+                    sizemode='area')))"""
 
-            fig.add_trace(go.Scattergeo(
+            fig.add_trace(go.Scattermapbox(
                 lat=[last_lat, locations['Latitude'][i+1]],
                 lon=[last_lon, locations['Longitude'][i+1]],
                 mode='lines',
-                line=dict(width=1, color='black'),
+                line=dict(width=1, color='green'),
             ))
-
             # Workaround to get the arrow at the end of an edge AB
 
-            l = 0.1  # the arrow length
-            widh = 0.035  # 2*widh is the width of the arrow base as triangle
+            l = 0.35  # the arrow length
+            widh = 0.045  # 2*widh is the width of the arrow base as triangle
 
             A = np.array([last_lon, last_lat])
             B = np.array([locations['Longitude'][i+1], locations['Latitude'][i+1]])
@@ -112,41 +113,74 @@ def vis2():
             S = P - widh * u
             T = P + widh * u
 
-            fig.add_trace(go.Scattergeo(lon=[S[0], T[0], B[0], S[0]],
+            fig.add_trace(go.Scattermapbox(lon=[S[0], T[0], B[0], S[0]],
                                         lat=[S[1], T[1], B[1], S[1]],
                                         mode='lines',
                                         fill='toself',
-                                        fillcolor='black',
-                                        line_color='black'))
+                                        fillcolor='green',
+                                        line_color='green'))
     
             last_lon = locations['Longitude'][i+1]
             last_lat = locations['Latitude'][i+1]
     fig.update_layout(
         showlegend=False,
-        geo=dict(
-            resolution = 50,
-            showland = True,
-            showlakes = True,
-            landcolor = 'rgb(204, 204, 204)',
-            countrycolor = 'rgb(204, 204, 204)',
-            lakecolor = 'rgb(255, 255, 255)',
-            projection_type = "equirectangular",
-            coastlinewidth = 2,
-            lataxis = dict(
-                range = [47, 54],
-                showgrid = True,
-                dtick = 10
-            ),
-            lonaxis = dict(
-                range = [8, 16],
-                showgrid = True,
-                dtick = 20
-            )
-        )
+        mapbox_style="stamen-terrain", 
+        mapbox_zoom=5, 
+        mapbox={"accesstoken":"pk.eyJ1IjoiYmV0dHliYWthbGltIiwiYSI6ImNreTBjdjE1cjAwZ3EyeG1ra3N4b3dzbXMifQ.-4CSgS7COsKTYD7aB2l04Q",'style': "outdoors"},
+        mapbox_center_lat = 52
     )
     fig.show()
 
-vis2()
+
+def vis3():
+    locations = pd.read_csv("route2.csv", delimiter=";")
+    locations["Latitude"] = locations["Latitude"].apply(lambda x: float(x.replace(",", ".")))
+    locations["Longitude"] = locations["Longitude"].apply(lambda x: float(x.replace(",", "."))) 
+    
+    selected = locations.copy()
+    locations.dropna(subset = ["Ort"], inplace=True)
+    locations.reset_index(drop=True, inplace=True)
+    
+    selected["Ort"] = selected["Ort"].apply(lambda x: str(x) )
+    def getOrt(x):
+        if x == "nan":
+            return ""
+        return x
+    selected["Ort"] = selected["Ort"].apply(lambda x: getOrt(x) )
+    scale = 5000
+    fig = go.Figure(go.Scattermapbox(lon=locations["Longitude"],lat=locations["Latitude"],mode="markers+text",marker={"size":15,"color":"red"},name="Route",text=locations["Ort"],textfont={"color":"black","size":10},textposition="bottom right"))
+    fig.add_trace(go.Scattermapbox(lon=selected["Longitude"],lat=selected["Latitude"],mode="markers",marker={"size":4,"color":"red"}))
+
+    for i in range(5,len(selected)-1):
+        if i % 1300 == 0:
+            # Workaround to get the arrow at the end of an edge AB
+            l = 0.15  # the arrow length
+            widh = 0.25  # 2*widh is the width of the arrow base as triangle
+
+            A = np.array([selected['Longitude'][i-300], selected['Latitude'][i-300]])
+            B = np.array([selected['Longitude'][i], selected['Latitude'][i]])
+            v = B - A
+            w = v / np.linalg.norm(v)
+            u = np.array([-v[1], v[0]])  # u orthogonal on  w
+
+            P = B - l * w
+            S = P - widh * u
+            T = P + widh * u    
+            fig.add_trace(go.Scattermapbox(lon=[S[0], T[0], B[0], S[0]],
+                                        lat=[S[1], T[1], B[1], S[1]],
+                                        mode='lines',
+                                        fill='toself',
+                                        fillcolor='green',
+                                        line_color='green'))
+    fig.update_layout(
+        showlegend=False,
+        mapbox_style="stamen-terrain", 
+        mapbox_zoom=5, 
+        mapbox={"accesstoken":"pk.eyJ1IjoiYmV0dHliYWthbGltIiwiYSI6ImNreTBjdjE1cjAwZ3EyeG1ra3N4b3dzbXMifQ.-4CSgS7COsKTYD7aB2l04Q",'style': "outdoors"},
+        mapbox_center_lat = 52
+    )
+    fig.show()
+vis3()
 #### Ab hier so b und c
 
 df = pd.read_csv('route2.csv', delimiter=";")
