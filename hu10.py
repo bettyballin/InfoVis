034b9cdc40320c45420a14f7910bc12afb8a542c
betty.ballin@yahoo.de <betty.ballin@yahoo.de>
2022-01-24 15:38:03 +0100

    cluster size

--- hu10.py
+++ hu10.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.manifold import MDS
 import numpy as np
+import plotly.graph_objects as go
 import plotly.express as px
 from matplotlib import pyplot as plt
 from matplotlib.collections import LineCollection
@@ -46,15 +47,21 @@ print()
 def ex2(jaccard):
     mds = MDS(dissimilarity="precomputed")
     transformed = mds.fit_transform(jaccard)
-
-    print(np.array(cluster_data[0]).shape)
-    print(np.array(cluster_data[1]).shape)
-    print(np.array(cluster_data[2]).shape)
-
-    fig = plt.figure()
     
-    df = pd.DataFrame({"x":transformed[:,0], "y":transformed[:,1], "text":["C3,1","C3,2","C3,3","C4,1","C4,2","C4,3","C4,4","C5,1","C5,2","C5,3","C5,4","C5,5"]})
-    fig = px.scatter(df, x="x", y="y", text="text", color="text", size_max=1.2)
+    fig = plt.figure()
+    c = cluster_data
+    df = pd.DataFrame({"x":transformed[:,0], "y":transformed[:,1], "len":[len(x)/10 for x in [c[0][0],c[0][1],c[0][2],c[1][0],c[1][1],c[1][2],c[1][3],c[2][0],c[2][1],c[2][2],c[2][3],c[2][4]]], "text":["C3,1","C3,2","C3,3","C4,1","C4,2","C4,3","C4,4","C5,1","C5,2","C5,3","C5,4","C5,5"]})
+    #fig = px.scatter(df, x="x", y="y", text="text", color="text", size="len", size_max=1.2)
+    
+    fig = go.Figure(data=go.Scatter(
+        x=transformed[:,0],
+        y=transformed[:,1],
+        mode='text+markers',
+        text=df["text"],
+        marker=dict(size=df["len"],
+                    color=list(range(12))),
+    ))
+
     fig.update_traces(textposition='top center')
     fig.update_layout(title_text='k-Means Cluster 10-Kampf', title_x=0.5)
     fig.show()
